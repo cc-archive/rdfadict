77b0ce2b4ec81c03ca6d8d53e3db376a8841abe3
nyergler <nyergler@committer.creativecommons.org>
2006-11-21 15:11:24 +0000

    Initial import of rdfadict into Subversion.

---
+++ setup.py
@@ -0,0 +1,46 @@
+## Copyright (c) 2006 Nathan R. Yergler, Creative Commons
+
+## Permission is hereby granted, free of charge, to any person obtaining
+## a copy of this software and associated documentation files (the "Software"),
+## to deal in the Software without restriction, including without limitation
+## the rights to use, copy, modify, merge, publish, distribute, sublicense,
+## and/or sell copies of the Software, and to permit persons to whom the
+## Software is furnished to do so, subject to the following conditions:
+
+## The above copyright notice and this permission notice shall be included in
+## all copies or substantial portions of the Software.
+
+## THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+## IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+## FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+## AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+## LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+## FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+## DEALINGS IN THE SOFTWARE.
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name = "rdfadict",
+    version = "0.1",
+    packages = ['rdfadict'],
+    package_dir = {'':'src'},
+
+    # scripts and dependencies
+    install_requires = ['setuptools',
+                        'lxml',
+                        ],
+    include_package_data = True,
+    zip_safe = True,
+
+    # author metadata
+    author = 'Nathan R. Yergler',
+    author_email = 'nathan@creativecommons.org',
+    description = 'Simple RDFa parser and dictionary-like interface.',
+    license = 'MIT',
+    url = 'http://wiki.creativecommons.org/RdfaDict',
+
+    )
