7f0f03a02512d25cc4dcc2d26cc345e56cd97d47
nyergler <nyergler@committer.creativecommons.org>
2008-07-31 21:19:04 +0000

    Initial work in implementing fully compliant parser; we pass about 50% of the tests at this point.

--- src/rdfadict/rdfa.py
+++ src/rdfadict/rdfa.py
@@ -22,8 +22,8 @@ import urllib
 import urlparse
 
 import lxml.etree
+import RDF
 
-import rdfadict.tidy
 from rdfadict.sink import DictTripleSink
 
 class SubjectResolutionError(AttributeError):
@@ -33,7 +33,13 @@ class SubjectResolutionError(AttributeError):
 class RdfaParser(object):
 
     HTML_RESERVED_WORDS = ('license',)
-
+    XHTML_VOCAB_NS = "http://www.w3.org/1999/xhtml/vocab#"
+    XHTML_REL_VALUES = ('alternate', 'appendix',
+                        'bookmark', 'cite', 'chapter', 'contents',
+                        'copyright', 'first', 'glossary', 'help', 'icon',
+                        'index', 'last', 'license', 'meta', 'next', 
+                        'p3pv1', 'prev', 'role', 'section', 'stylesheet',
+                        'subsection', 'start', 'top', 'up')
     def __init__(self):
 
         self.reset()
@@ -45,7 +51,7 @@ class RdfaParser(object):
         self.__bnodes = {}
 
         # we default the cc: namespace to Creative Commons
-        self.__nsmap = {'cc':'http://web.resource.org/cc/'}
+        self.__nsmap = {'cc':'http://creativecommons.org/ns#'}
         
 ##        self.__nsmap = {None:'http://www.w3.org/1999/xhtml',
 ##                        }
@@ -72,14 +78,16 @@ class RdfaParser(object):
             lxml_doc = lxml.etree.fromstring(in_string)
         except lxml.etree.XMLSyntaxError, e:
 
-            try:
-                # try to Tidy the HTML
-                lxml_doc = rdfadict.tidy.tidystring(in_string).getroot()
-            except:
-                # try to parse as HTML
-                lxml_doc = lxml.etree.fromstring(in_string,
-                                                 lxml.etree.HTMLParser())
+            # try to parse as HTML
+            lxml_doc = lxml.etree.fromstring(in_string,
+                                             lxml.etree.HTMLParser())
 
+        # see if there's an explicit base set in the document;
+        # if so, it overrides the URL specified as the base
+        if lxml_doc.xpath('//head/base'):
+            base_uri = lxml_doc.xpath('//head/base')[-1].attrib['href']
+
+        # call the primary parser
         return self.__parse(lxml_doc, base_uri, sink)
 
     def parseurl(self, url, sink=None):
@@ -105,8 +113,8 @@ class RdfaParser(object):
                 explicit_parent = False
 
             if explicit_parent:
-                return self.__resolve_uri(explicit_parent.attrib['about'],
-                                          base_uri)
+                return self.__resolve_safeCurie(
+                    explicit_parent.attrib['about'], base_uri)
             else:
                 # XXX Does not handle head in XHTML2 docs; see 4.3.3.1 in spec
                 # no explicitly defined parent, perform reification
@@ -116,8 +124,8 @@ class RdfaParser(object):
         # traverse up tree looking for an about tag
         about_nodes = node.xpath('ancestor-or-self::*[@about]')
         if about_nodes:
-            return self.__resolve_uri(about_nodes[-1].attrib['about'],
-                                      base_uri)
+            return self.__resolve_safeCurie(about_nodes[-1].attrib['about'],
+                                            base_uri, about_nodes[-1])
         else:
             return None
 
@@ -127,36 +135,20 @@ class RdfaParser(object):
 
         return urlparse.urljoin(base_uri, uri)
 
-    def __resolve_curie(self, curie_or_uri, context=None):
-        """Convert a compact URI (i.e., "cc:license") to a fully-qualified
-        URI.  [context] is an Element or None.  If it is not None, it will
-        be used to resolve local namespace declarations.  If it is None,
-        only the namespaces declared as part of the root element will be
-        available.
-        """
-        
-        # is this already a uri?
-        url_pieces = urlparse.urlparse(curie_or_uri)
-        if '' not in [url_pieces[0], url_pieces[1]]:
+    def __resolve_safeCurie(self, safe_curie, base_uri, context):
 
-            # already a valid URI
-            return curie_or_uri
+        if not safe_curie: return None
 
-        # is this a urn?
-        if (len(curie_or_uri) >= 4) and curie_or_uri.lower()[:4] == "urn:":
-            return curie_or_uri
+        if safe_curie[0] == '[' and safe_curie[-1] == ']':
+            return self.__resolve_curie(safe_curie[1:-1], context)
+        else:
+            # XXX
+            return self.__resolve_uri(safe_curie, base_uri)
 
-        # determine if this CURIE has a namespace
-        if ":" not in curie_or_uri:
-            # no namespace; if this isn't a reserved word, we throw it away
-            if curie_or_uri.lower() not in self.HTML_RESERVED_WORDS:
-                return None
-            else:
-                # reserved word; map it to the XHTML namespace
-                return "http://www.w3.org/1999/xhtml#%s" % curie_or_uri
+    def __resolve_curie(self, curie, context=None):
 
         # resolve it using our namespace map
-        ns, path = curie_or_uri.split(':', 1)
+        ns, path = curie.split(':', 1)
         if ns == '':
             ns = None
 
@@ -171,8 +163,8 @@ class RdfaParser(object):
         # if we were unable to resolve the namespace, return None
         if ns is None:
             # make sure this isn't "cc:license"
-            if curie_or_uri.lower().strip() == 'cc:license':
-                return self.__resolve_curie('license')
+            if curie.lower().strip() == 'cc:license':
+                return self.__resolve_relrev('license')
 
             return None
             
@@ -180,7 +172,49 @@ class RdfaParser(object):
             ns = "%s#" % ns
 
         return "%s%s" % (ns, path)
-    
+
+    def __resolve_relrev(self, curie_or_uri, context=None):
+        """Convert a compact URI (i.e., "cc:license") to a fully-qualified
+        URI.  [context] is an Element or None.  If it is not None, it will
+        be used to resolve local namespace declarations.  If it is None,
+        only the namespaces declared as part of the root element will be
+        available.
+        """
+        
+        """
+        # is this already a uri?
+        url_pieces = urlparse.urlparse(curie_or_uri)
+        if '' not in [url_pieces[0], url_pieces[1]]:
+
+            # already a valid URI
+            return curie_or_uri
+
+        # is this a urn?
+        if (len(curie_or_uri) >= 4) and curie_or_uri.lower()[:4] == "urn:":
+            return curie_or_uri
+        """
+
+        # determine if this CURIE has a namespace
+        if ":" not in curie_or_uri:
+            # no namespace; if this isn't a reserved word, we throw it away
+            if curie_or_uri.lower() in self.XHTML_REL_VALUES:
+                return "%s%s" % (self.XHTML_VOCAB_NS, curie_or_uri.lower())
+            else:
+                # not the XHTML vocabulary; no namespace, so no triple
+                return None
+        else:
+            return self.__resolve_curie(curie_or_uri, context)
+
+    def __get_content(self, node):
+        """Return the content of the node; content is returned as an
+        RDF.Node with appropriate language and datatype settings."""
+
+        content = node.attrib.get('content', node.text)
+        lang = node.attrib.get('{http://www.w3.org/XML/1998/namespace}lang', 
+                               '')
+
+        return RDF.Node(literal=content, language=lang)
+
     def __parse(self, lxml_doc, base_uri, sink):
 
         RDFA_ATTRS = ("about", "property", "rel", "rev", "href", "content")
@@ -196,13 +230,15 @@ class RdfaParser(object):
         for node in lxml_doc.xpath('//*[@property]'):
 
             subject = self.__resolve_subject(node, base_uri) or base_uri
-            obj = node.attrib.get('content', node.text)
+            obj = self.__get_content(node)
 
             for p in node.attrib.get('property').split():
-                pred = self.__resolve_curie(p, node)
+                pred = self.__resolve_relrev(p, node)
                 if pred is not None:
                     # the CURIE resolved
-                    sink.triple( subject, pred, obj )
+                    sink.triple( RDF.Uri(subject), RDF.Uri(pred), obj )
+                else:
+                    print obj
 
         # using rel
         for node in lxml_doc.xpath('//*[@rel]'):
@@ -215,17 +251,27 @@ class RdfaParser(object):
                 # are namespaced, this doesn't matter... so save it for later
                 subj_err = e
 
-            obj = self.__resolve_uri(node.attrib.get('href'), base_uri)
+            # look for resource, then href to complete the triple
+            obj = None
+            if 'resource' in node.attrib:
+                obj = self.__resolve_safeCurie(node.attrib.get('resource'),
+                                               base_uri, node)
+            elif 'href' in node.attrib:
+                obj = self.__resolve_uri(node.attrib.get('href'),
+                                         base_uri)
+            else:
+                # neither resource or href; nothing to do here
+                continue
 
             for p in node.attrib.get('rel').split():
-                pred = self.__resolve_curie(p, node)
+                pred = self.__resolve_relrev(p, node)
                 if pred is not None:
                     # the CURIE resolved -- 
                     # make sure we were able to resolve the subject
                     if subj_err is not None:
                         raise subj_err
 
-                    sink.triple( subject, pred, obj )
+                    sink.triple( RDF.Uri(subject), RDF.Uri(pred), RDF.Uri(obj) )
 
         # using rev
         for node in lxml_doc.xpath('//*[@rev]'):
@@ -238,16 +284,26 @@ class RdfaParser(object):
                 # are namespaced, this doesn't matter... so save it for later
                 obj_err = e
 
-            subject = self.__resolve_uri(node.attrib.get('href'), base_uri)
+            # look for resource, then href to complete the triple
+            subject = None
+            if 'resource' in node.attrib:
+                subject = self.__resolve_safeCurie(node.attrib.get('resource'),
+                                               base_uri, node)
+            elif 'href' in node.attrib:
+                subject = self.__resolve_uri(node.attrib.get('href'),
+                                         base_uri)
+            else:
+                # neither resource or href; nothing to do here
+                continue
 
             for p in node.attrib.get('rev').split():
-                pred = self.__resolve_curie(p, node)
+                pred = self.__resolve_relrev(p, node)
                 if pred is not None:
                     # the CURIE resolved -- 
                     # make sure we were able to resolve the subject
                     if obj_err is not None:
                         raise obj_err
 
-                    sink.triple( subject, pred, obj )
+                    sink.triple( RDF.Uri(subject), RDF.Uri(pred), RDF.Uri(obj) )
 
         return sink
